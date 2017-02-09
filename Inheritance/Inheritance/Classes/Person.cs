111b0fd2dcd8fa774b7b4385b6b83b9cea445304
user <user@DESKTOP-I6SPLCP>
2017-02-09 17:48:34 +0400

    Not completed

---
+++ Inheritance/Inheritance/Classes/Person.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Person
+    {
+        //protected string first_name;
+        private string firstname;
+        private string lastname;
+        private int age;
+        private int Id;
+
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+               firstname = value; }
+        }
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+
+           public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+        public int ID
+        {
+            get
+            {
+                return Id;
+            }
+            set
+            {
+                Id= value;
+            }
+        }
+        public Person(){ }
+        public Person(string firstname, string lastname, int age)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.age = age;
+        }
+        public string print
+        {
+            get
+            {
+               return firstname+" "+lastname+" "+age;
+            }
+        }
+
+
+    } 
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Person pr = new Person();
+            pr.Firstname = "Լիլիթ";
+            var gr=new Person("Գևորգ", "Թումանյան", 5);
+            Console.WriteLine(pr.Firstname);
+            Console.WriteLine(gr.print);
+            Console.Read();
+        }
+    }
+}
